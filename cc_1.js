899054876a73c5ef50a12d989535bffd8f9d5945
Divya Joshi <divyaj.redj@gmail.com>
2025-01-22 18:14:43 -0500

    Task 2 Completed

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+//Task 1//
 let employeeName = '';
 const employeeID = 12345;
 var isActive = true;
@@ -6,3 +7,11 @@ console.log("Employee Name:", employeeName, "- Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "- Type:", typeof employeeID);
 console.log("Is Active:", isActive, "- Type:", typeof isActive);
 
+//Task 2//
+let productName = 'Hoodie';
+const productPrice = 49.99;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "- Type:", typeof productName);
+console.log("Product Price:", productPrice, "- Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "- Type:", typeof isAvailable);
