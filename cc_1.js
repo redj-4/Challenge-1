0b535a14d9bd30b8c736b697fed4645193d6ca97
Divya Joshi <divyaj.redj@gmail.com>
2025-01-22 18:40:12 -0500

    "Task 3 - Financial Transactions."

--- cc_1.js
+++ cc_1.js
@@ -15,3 +15,20 @@ var isAvailable = true;
 console.log("Product Name:", productName, "- Type:", typeof productName);
 console.log("Product Price:", productPrice, "- Type:", typeof productPrice);
 console.log("Is Available:", isAvailable, "- Type:", typeof isAvailable);
+
+//Task 3//
+let accountBalance = 1000;
+
+accountBalance += 500;
+console.log(accountBalance)
+
+accountBalance -=200;
+console.log(accountBalance)
+
+accountBalance *=1.5;
+console.log(accountBalance)
+
+accountBalance /=2
+console.log(accountBalance)
+
+//Task 4//
