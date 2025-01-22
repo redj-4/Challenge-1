84cbbc0c4b248c6fdaaeeee28d83b8e53b66ead5
Divya Joshi <divyaj.redj@gmail.com>
2025-01-22 18:08:09 -0500

    Task 1 Completed

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+let employeeName = '';
+const employeeID = 12345;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "- Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "- Type:", typeof employeeID);
+console.log("Is Active:", isActive, "- Type:", typeof isActive);
+
